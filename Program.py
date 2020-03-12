4c336b971e1c4690017df1740fc3aedc68d8445b
Felipe Berger <feconchaber@gmail.com>
2020-03-12 12:16:24 +1100

    Update Program.py
    Modified the following:
    - updated all functions to use CSV instead of regular read/write functionality
    - added error handling for input prompts
    - time spent with the timer function is now tracked for each task

--- Program.py
+++ Program.py
@@ -1,5 +1,6 @@
 import to_do
 import timer
+import csv
 
 print("###########################################")
 print("            to-do list program")
@@ -10,14 +11,14 @@ print()
 def run_event_loop():
     print("What would you like to do?")
     cmd = "EMPTY"
-    timer_counter = 0
 
     while cmd != "x" and cmd:
         cmd = input("\n[L]ist tasks, [A]dd task, [R]emove task, [S]tart timer, E[x]it? ")
         cmd = cmd.lower().strip()
         if cmd == "l":
+            print("Labels: Task No, Task, Time Spent")
             for idx, tsk in enumerate(to_do.read_tasks(), start=1):
-                print(idx, "- ", tsk)
+                print(idx, "-", "{}, {} minutes".format(tsk[0], tsk[1]))
 
         # let's users add a task to the to-do list
         elif cmd == "a":
@@ -25,27 +26,45 @@ def run_event_loop():
             to_do.add_task(new_task)
             print("\nUpdated task list:")
             for idx, tsk in enumerate(to_do.read_tasks(), start=1):
-                print(idx, "- ", tsk)
+                print(idx, "-", "{}, {} minutes".format(tsk[0], tsk[1]))
 
         # let's users remove a task from the to-do list
         elif cmd == "r":
-            remove_task = int(input("\nEnter number of task you want to remove: "))
+            while True:
+                try:
+                    remove_task = int(input("\nEnter number of task you want to remove: "))
+                    break
+                except ValueError:
+                    print("Not a valid number, please try again")
             for idx, tsk in enumerate(to_do.read_tasks(), start=1):
                 if idx == remove_task:
                     to_do.remove_task(tsk)
             print("\nUpdated task list:")
             for idx, tsk in enumerate(to_do.read_tasks(), start=1):
-                print(idx, "- ", tsk)
+                print(idx, "-", "{}, {} minutes".format(tsk[0], tsk[1]))
 
         # let's the user create a work countdown timer and a rest timer
-        # prints the number of timers completed so far
         elif cmd == "s":
-            work = int(input("\nHow long [minutes] do you want to work for?: "))
-            rest = int(input("How long [minutes] do you want to rest for?: "))
-            print("\n")
+            while True:
+                try:
+                    task_selected = int(input("\nWhat task do you want to work on? [task number]: "))
+                    work = int(input("How many minutes do you want to work for?: "))
+                    rest = int(input("How many minutes do you want to rest for?: "))
+                    print("\n")
+                    break
+                except ValueError:
+                    print("Wrong input, please try again")
             timer.pomodoro(work, rest)
-            timer_counter += 1
-            print("You've completed {} timers so far!".format(timer_counter))
+            time_worked = work
+            data_update = to_do.read_tasks()
+            with open("task_file.txt", "w", newline='') as task_file:
+                csv_writer = csv.writer(task_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                for idx, tsk in enumerate(data_update, start=1):
+                    if idx == task_selected:
+                        time_worked += int(tsk[1])
+                        tsk[1] = time_worked
+                for row in data_update:
+                    csv_writer.writerow(row)
 
         # catches any input that is not one of the expected ones and outputs error message
         elif cmd != "x" and cmd:

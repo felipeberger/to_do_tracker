30f560678cead283dd0a598a9c99c147a2997ca4
Felipe Berger <feconchaber@gmail.com>
2020-03-12 12:19:25 +1100

    Update to_do.py
    Updated the following:
    - CSV now used for all methods

--- to_do.py
+++ to_do.py
@@ -1,25 +1,27 @@
-# print("###########################################")
-# print("         to-do file read and write")
-# print("###########################################")
-# print()
+import csv
 
 
 # Opens the .txt file where tasks are stored, saves them to memory, and prints them to the console
 def read_tasks():
     task_data = []
     with open("task_file.txt", "r") as task_file:
-        for line in task_file.readlines():
-            task_data.append(line.rstrip())
+        csv_reader = csv.reader(task_file, delimiter=',')
+        for row in csv_reader:
+            task_data.append(row)
     return task_data
 
 
 # Appends entry to the task_file
 def add_task(task):
-    if task not in read_tasks():
-        with open("task_file.txt", "a") as task_file:
-            task_file.write("\n" + str(task))
+    add_task_data = read_tasks()
+    if task not in add_task_data:
+        add_task_data.append([task, "0"]) # adds a zero after task, which will be used for the time-tracking function
     else:
         print("\nError: task already in to-do list\n")
+    with open("task_file.txt", "w", newline='') as task_file:
+        csv_writer = csv.writer(task_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in add_task_data:
+            csv_writer.writerow(row)
 
 
 # removes the task input if it exists in the file. Rewrites the file without the removed task
@@ -27,11 +29,9 @@ def remove_task(task):
     remove_data = read_tasks()
     if task in remove_data:
         remove_data.remove(task)
-        with open("task_file.txt", "w") as task_file:
-            for counter, line in enumerate(remove_data):
-                if not counter:
-                    task_file.write(line)
-                elif counter:
-                    task_file.write("\n" + line)
+        with open("task_file.txt", "w", newline='') as task_file:
+            csv_writer = csv.writer(task_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for row in remove_data:
+                csv_writer.writerow(row)
     else:
         print("\nError: Task not in to-do list\n")
